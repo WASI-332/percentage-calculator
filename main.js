b72751a28a5dff3aefb05e30d63b1827f1ba1737
WASI-332 <161990667+WASI-332@users.noreply.github.com>
2024-12-28 10:59:17 +0500

    created index.html

---
+++ main.js
@@ -0,0 +1,7 @@
+var total = prompt("Enter your Total Marks");
+
+var obtained = prompt("Enter your Obtained Marks");
+
+var percentage = (obtained/total)*100;
+
+alert ("Your Percentage is " + percentage + "%");
